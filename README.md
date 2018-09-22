def2e0f340b1ec96abd494df2dd194b56ab1aa22
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2018-09-23 02:08:28 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # ExchangeIF
-one of Esolang using EXIF Data.
+one of Esolang using EXIF Data. It works on hakatashi/esolang-box.
+
+### How to Use
+prepare pictures, and zip these pictures. and
+```bash
+ExchangeIF [zipfile]
+```
+to execute.
+
+### Documentation
+取り敢えず日本語で書いておく。
+
+* ルールその1	プログラムは作成日時の早い順に実行されていきます。同時刻のファイルがある際の動作はランダムです。
+* ルールその2	変数はGPS座標で識別します。latitude, longitudeで識別します。各変数には数値が1つだけ入ります。
+* ルールその3	1コマンドは1つの画像からなり、コマンド及びその文法については以下のようになっています。
+
+コマンド	条件			影響先	影響元
+代入		ISOが0~99	GPS(lat)		画像サイズ（縦×横がそのまま入る）
+入力		ISOが100~199	GPS(lat)		無し（標準入力から1文字文字コードとして受け取ります）
+出力		ISOが200~299	無し（標準出力へ文字コード1文字）		GPS(lng)
+加算		ISOが300~399	GPS(lat)		GPS(lng)
+減算		ISOが400~499	GPS(lat)		GPS(lng)
+乗算		ISOが500~599	GPS(lat)		GPS(lng)
+除算		ISOが600~699	GPS(lat)		GPS(lng)
+剰余		ISOが700~799	GPS(lat)		GPS(lng)
+
+* ルールその4	タイムトラベルが可能です。
+タイムトラベル		
+ただし、タイムトラベルの代償として「現在時刻との差の絶対値の時間」を無為に消費します。時間切れに気を付けましょう。
+ISOコマンドとタイムトラベルの優先順位はタイムトラベルが上です。
+
+ルールその5	GPS座標はlat、lng共通です。Metering Modeを2進数として見たとき、下2桁について、
+下1桁が1→lngはlngが示す値の変数を参照します
+下2桁目が1→latはlatが示す値の変数を参照します
+
+実質bfなので面白みはありません。変則ショートコーティングに挑んでください。
+
+### Samples
+* HelloWorld
+面倒だったのでHだけ出力。許してくだせぇ…
+* cat
---
+++ exif.py
@@ -0,0 +1,123 @@
+import sys
+from datetime import datetime
+import random
+import bisect
+import time
+import zipfile
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+argv = sys.argv
+if len(argv) < 0:
+	print("Usage: python exif.py [filename]")
+path = argv[1]
+
+programs = {}
+with zipfile.ZipFile(path) as zipObj:
+	zipObj.extractall("./")
+	for filename in zipObj.namelist():
+		if filename[-1] == "/":
+			continue
+		img = Image.open(filename)
+		exif = img._getexif()
+		DateTimeOriginal = ""
+		DateTime = ""
+		MeteringMode = 0
+		command = ""
+		lat = 0.0
+		lng = 0.0
+		height = 0
+		width = 0
+		for key, val in exif.items():
+			if TAGS.get(key, key) == "DateTimeOriginal":
+				DateTimeOriginal = datetime.strptime(val, "%Y:%m:%d %H:%M:%S")
+			elif TAGS.get(key, key) == "DateTime":
+				DateTime = datetime.strptime(val, "%Y:%m:%d %H:%M:%S")
+			elif TAGS.get(key, key) == "MeteringMode":
+				MeteringMode = val
+			elif TAGS.get(key, key) == "ISOSpeedRatings":
+				if val < 100:
+					command = "substitution"
+				elif val < 200:
+					command = "input"
+				elif val < 300:
+					command = "output"
+				elif val < 400:
+					command = "addition"
+				elif val < 500:
+					command = "substraction"
+				elif val < 600:
+					command = "multiplication"
+				elif val < 700:
+					command = "division"
+				elif val < 800:
+					command = "remainder"
+			elif TAGS.get(key, key) == "ExifImageHeight":
+				height = val
+			elif TAGS.get(key, key) == "ExifImageWidth":
+				width = val
+			elif TAGS.get(key, key) == "GPSInfo":
+				lat = (1 if val[1] == "N" else - 1) * (1.0 * val[2][0][0] /
+														val[2][0][1] + 1.0 / 60 * val[2][1][0] / val[2][1][1])
+				lng = (1 if val[3] == "E" else - 1) * (1.0 * val[4][0][0] /
+														val[4][0][1] + 1.0 / 60 * val[4][1][0] / val[4][1][1])
+		if DateTimeOriginal in programs:
+			programs[DateTimeOriginal].append({"DateTimeOriginal": DateTimeOriginal,
+												"DateTime": DateTime,
+												"MeteringMode": MeteringMode,
+												"command": command,
+												"height": height,
+												"width": width,
+												"latitude": lat,
+												"longitude": lng
+												})
+		else:
+			programs[DateTimeOriginal] = [{"DateTimeOriginal": DateTimeOriginal,
+											"DateTime": DateTime,
+											"MeteringMode": MeteringMode,
+											"command": command,
+											"height": height,
+											"width": width,
+											"latitude": lat,
+											"longitude": lng
+											}]
+
+curTime = min(programs.keys())
+times = sorted(list(programs.keys()))
+def getNextTime(curTime):
+	if curTime >= max(programs.keys()):
+		return None
+	return times[bisect.bisect_right(times, curTime)]
+data = {}
+
+while True:
+	target = programs[curTime][0]
+	if target["DateTime"] != target["DateTimeOriginal"]:
+		curTime = target["DateTime"]
+		time.sleep(abs((curTime - target["DateTimeOriginal"]).total_seconds())/20)
+	else:
+		addrfrom = target["longitude"]
+		if target["MeteringMode"]&1:
+			addrfrom = data[target["longitude"]]
+		addrto = target["latitude"]
+		if target["MeteringMode"]&2:
+			addrto = data[target["latitude"]]
+		if target["command"] == "substitution":
+			data[addrto] = target["width"] * target["height"]
+		elif target["command"] == "input":
+			data[addrto] = ord(sys.stdin.buffer.read(1))
+		elif target["command"] == "output":
+			sys.stdout.buffer.write(chr(data[addrfrom]).encode("ascii"))
+		elif target["command"] == "addition":
+			data[addrto] += data[addrfrom]
+		elif target["command"] == "substraction":
+			data[addrto] -= data[addrfrom]
+		elif target["command"] == "multiplicatoin":
+			data[addrto] *= data[addrfrom]
+		elif target["command"] == "division":
+			data[addrto] //= data[addrfrom]
+		elif target["command"] == "remainder":
+			data[addrto] %= data[addrfrom]
+	curTime = getNextTime(curTime)
+	if curTime is None:
+		break
